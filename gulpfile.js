c01b44ae96116c7e73f329ee3959c7c16932b744
Matt McDaniel <matt.h.mcdaniel@gmail.com>
2016-03-28 20:26:29 -0700

    Update browsersync settings

--- gulpfile.js
+++ gulpfile.js
@@ -9,20 +9,12 @@ var browserSync = require('browser-sync').create();
 
 var ENV = process.env.NODE_ENV;
 
-function build() {
-    
-    gulp.src(['src/templates/*.html'])
-        .pipe(include())
-        .pipe(replace(/public\/([a-z1-9\-\_]*)\.html/gi, '$1.html'))
-        .pipe(gulp.dest('public'));
-        
-}
-
 // delete build folder
 gulp.task('del', function() {
-    del('public/*.html');
+    del('public/**/*');
 });
 
+// build public/main.css
 gulp.task('styles', function() {
     gulp.src('src/scss/main.scss')
         .pipe(sass().on('error', sass.logError))
@@ -30,16 +22,32 @@ gulp.task('styles', function() {
         .pipe(browserSync.stream());
 });
 
-gulp.task('build', build);
+// build src/**/*.html into public/*.html
+gulp.task('build', function() {
+    gulp.src(['src/templates/*.html'])
+        .pipe(include())
+        .pipe(replace(/public\/([a-z1-9\-\_]*)\.html/gi, '$1.html'))
+        .pipe(gulp.dest('public'));
+});
 
 gulp.task('serve', function() {
+    
     browserSync.init({
-        server: './public'
+        server: {
+            baseDir: './public',
+        },
+        notify: false,
+        reloadOnRestart: true,
+        open: false,
+        ui: false
     });
     
-    gulp.watch(['src/**/*.js', 'src/**/*.html', 'src/**/*.scss'], ['del', 'styles', 'build']);
-    gulp.watch(['src/**/*.js', 'src/**/*.html']).on('change', browserSync.reload);
-})
+    gulp.watch(['src/**/*.scss'], ['styles']);
+    gulp.watch(['src/**/*.html'], ['build']);
+    
+    // fire reload when public files change
+    gulp.watch(['public/*.html', 'public/*.css']).on('change', browserSync.reload);
+});
 
 //NODE_ENV=development
 gulp.task('development', ['serve']);
