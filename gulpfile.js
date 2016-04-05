d701c930a05b15496b41eeb5e0cda9d8b1ce6089
Matt McDaniel <matt.h.mcdaniel@gmail.com>
2016-04-04 22:11:31 -0700

    commit v1

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,23 @@ var debug = require('gulp-debug');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 var ENV = process.env.NODE_ENV;
 
 // delete build folder
 gulp.task('del', function() {
-    del('public/**/*');
+    del('index.html, main.css, main.bundle.js');
 });
 
 // build public/main.css
 gulp.task('styles', function() {
     gulp.src('src/scss/main.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('public'))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('.'))
         .pipe(browserSync.stream());
 });
 
@@ -26,15 +30,19 @@ gulp.task('styles', function() {
 gulp.task('build', function() {
     gulp.src(['src/templates/*.html'])
         .pipe(include())
-        .pipe(replace(/public\/([a-z1-9\-\_]*)\.html/gi, '$1.html'))
-        .pipe(gulp.dest('public'));
+        //.pipe(replace(/public\/([a-z1-9\-\_]*)\.html/gi, '$1.html'))
+        .pipe(gulp.dest('.'));
+        
+    gulp.src(['src/js/*.js'])
+        .pipe(concat('main.bundle.js'))
+        .pipe(gulp.dest('.'));
 });
 
 gulp.task('serve', function() {
     
     browserSync.init({
         server: {
-            baseDir: './public',
+            baseDir: './',
         },
         notify: false,
         reloadOnRestart: true,
@@ -43,10 +51,14 @@ gulp.task('serve', function() {
     });
     
     gulp.watch(['src/**/*.scss'], ['styles']);
-    gulp.watch(['src/**/*.html'], ['build']);
+    gulp.watch(['src/**/*.html', 'src/js/*.js'], ['build']);
     
     // fire reload when public files change
-    gulp.watch(['public/*.html', 'public/*.css']).on('change', browserSync.reload);
+    gulp.watch([
+        '*.html', 
+        '*.css',
+        'src/img/*.svg'
+    ]).on('change', browserSync.reload);
 });
 
 //NODE_ENV=development
