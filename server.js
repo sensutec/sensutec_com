77b863cade496e4657e4e543384a85eaf1db5223
Matt McDaniel <matt.h.mcdaniel@gmail.com>
2016-04-17 19:31:54 -0700

    add static build url for images

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.get('/', function(req, res) {
 });
 
 // Enable Express to get dependency scripts
-app.use('/', express.static(__dirname + '/'));
+app.use('/', express.static(__dirname + '/public'));
+app.use('/img', express.static(__dirname + '/img'));
 
 app.listen(port, function(error) {
 	if (!error) {
