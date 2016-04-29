6379d57322b634072f16c59584860dbad437703f
Matt McDaniel <matt.h.mcdaniel@gmail.com>
2016-04-28 21:16:46 -0700

    Fix corext content

--- app/components/cortext/Cortext.js
+++ app/components/cortext/Cortext.js
@@ -9,10 +9,10 @@ class Cortext extends React.Component {
             <div className="content-body">
                 <h1>Stop Searching. Start Finding.</h1>
                 <p>
-                    Research is a tedious process. Large volumes of unstructured data, distributed across multiple sources have made finding useful information harder than ever.
+                    Research is a tedious process. Large volumes of unstructured data, distributed across multiple sources, have made finding useful information harder than ever.
                 </p>
                 <p>
-                    Our web application, Cortext, uses semantic data interpretation to help you gather trends, hone in on specifics, and determine paths forward. You can produce a wide range of useful visualizations, as well as output data in many common data formats (like CSV and JSON). This data becomes yours to use to share with friends, include in a report, or take on your next big project.
+                    Our web application, Cortext, uses semantic data interpretation to help you gather trends, hone in on specifics, and determine paths forward. You can produce a wide range of useful visualizations, as well as output data in many common data formats (like CSV and JSON). This data becomes yours to include in a report or use to take on your next big project.
                 </p>
             </div>
         )
